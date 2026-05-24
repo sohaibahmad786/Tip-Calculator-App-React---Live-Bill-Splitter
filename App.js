8d199af29e14f15ef79b4d71e80a348ec165cc0c
Sohaib Ahmad <sohaibahmad33888237@gmail.com>
2026-05-24 17:36:51 +0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tip_calculator from './Dev Weakend/Tip_calculator'
+
+const App = () => {
+  return (
+    <div>
+      <Tip_calculator />
+    </div>
+  )
+}
+
+export default App
